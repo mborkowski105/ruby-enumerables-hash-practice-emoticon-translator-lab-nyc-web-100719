7997cdf03756f43c225c38e314c369ad1fac2e2d
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-05 03:42:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,19 +9,26 @@ def load_library(file_path)
   raw_data.each do |emotion, emoticon_array|
     jpn_emoticon = emoticon_array[1]
     eng_emoticon = emoticon_array[0]
-    data['get_meaning'][jpn_emoticon] = eng_emoticon 
+    data['get_meaning'][jpn_emoticon] = emotion
+    data['get_emoticon'][eng_emoticon] = jpn_emoticon
   end
   
-  puts raw_data["angel"]
-  puts " "
-  puts data['get_meaning']
   return data
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
   # code goes here
+  data = load_library(file_path)
+  
+  data['get_emoticon'].each do |face, emotion|
+    if (emoticon == face)
+      return emotion
+    end
+  end
+  return "Sorry!"
 end
 
 def get_english_meaning
   # code goes here
+end
 end
