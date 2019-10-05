bdab7730b8f4318e4fdc904d11ac2d01b85adaad
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-05 03:42:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,21 @@
 # require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  raw_data = YAML.load_file(file_path)
+  
+  data = {'get_meaning' => {}, 'get_emoticon' => {}}
+  
+  raw_data.each do |emotion, emoticon_array|
+    jpn_emoticon = emoticon_array[1]
+    eng_emoticon = emoticon_array[0]
+    data['get_meaning'][jpn_emoticon] = eng_emoticon 
+  end
+  
+  puts raw_data["angel"]
+  puts " "
+  puts data['get_meaning']
+  return data
 end
 
 def get_japanese_emoticon
